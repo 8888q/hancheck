3cc6afeed1db4117cce9f189cfbc7e0b32f8161a
jake <jake@propaya.com>
2026-02-06 03:35:32 -0800

    router basename

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 //   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/hancheck">
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="/daily" element={<Daily />} />
